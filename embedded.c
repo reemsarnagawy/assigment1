5c2a6d08437df3bf489f8669ccbee36571a3b81f
reemsarnagawy <82292519+reemsarnagawy@users.noreply.github.com>
2021-04-19 02:00:18 +0200

    Add files via upload

---
+++ embedded.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+struct student {
+    char firstName[50];
+    int date;
+    int ID;
+    int score
+} ;
+
+int main() {
+
+    printf("how are you\n");
+
+
+
+    return 0;
+}
