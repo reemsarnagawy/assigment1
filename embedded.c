2736f0595fee3b00f9f04d9d3a544b9ba9174cdf
reemsarnagawy <82292519+reemsarnagawy@users.noreply.github.com>
2021-05-01 17:09:49 +0200

    Update embedded.c

--- embedded.c
+++ embedded.c
@@ -1,17 +1,13 @@
-
 #include <stdio.h>
+#include <stdlib.h>
+struct birth {
+int day;
+int month;
+int year;
+}bir;
 struct student {
-    char firstName[50];
-    int date;
-    int ID;
-    int score
-} ;
-
-int main() {
-
-    printf("how are you\n");
-
-
-
-    return 0;
-}
+    char name [50];
+    int id;
+    int score_last_year;
+    struct birth bir;
+}data;
